538640a7aa0815d24fd3742220bb15a0cdd527bb
Nicolas Bastien <nbastien@prestaconcept.net>
2014-03-03 16:18:51 +0100

    Add base models and admin extension for Gedmo implementation

---
+++ Model/Gedmo/AbstractTranslation.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sonata\TranslationBundle\Model\Gedmo;
+
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+/**
+ * @author Nicolas Bastien <nbastien.pro@gmail.com>
+ * @see https://github.com/l3pp4rd/DoctrineExtensions/blob/master/doc/translatable.md : Personal translations
+ */
+class AbstractTranslation extends AbstractPersonalTranslation
+{
+    /**
+     * Convenient constructor
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale = null, $field = null, $value = null)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+}
