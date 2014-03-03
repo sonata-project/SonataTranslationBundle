55fbe4309028f95d103beea734ee728b590c9687
Nicolas Bastien <nbastien@prestaconcept.net>
2014-03-03 14:47:12 +0100

    Base admin extension for translated models

---
+++ Admin/Extension/AbstractTranslatableAdminExtension.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sonata\TranslationBundle\Admin\Extension;
+
+use Sonata\AdminBundle\Admin\AdminExtension;
+use Sonata\AdminBundle\Admin\AdminInterface;
+
+/**
+ * @author Nicolas Bastien <nbastien.pro@gmail.com>
+ */
+abstract class AbstractTranslatableAdminExtension extends AdminExtension
+{
+    /**
+     * Request parameter
+     */
+    const TRANSLATABLE_LOCALE_PARAMETER = 'tl';
+
+    /**
+     * @return ContainerInterface
+     */
+    protected function getContainer(AdminInterface $admin)
+    {
+        return $admin->getConfigurationPool()->getContainer();
+    }
+
+    /**
+     * Return the list of possible locales for your models
+     *
+     * @return array
+     */
+    protected function getTranslationLocales(AdminInterface $admin)
+    {
+        return $this->getContainer($admin)->getParameter('sonata_translation.locales');
+    }
+
+    /**
+     * Return the default locale if url parameter is not present
+     *
+     * @return string
+     */
+    protected function getDefaultTranslationLocale(AdminInterface $admin)
+    {
+        return $this->getContainer($admin)->getParameter('sonata_translation.default_locale');
+    }
+
+    /**
+     * Return current translatable locale
+     * ie: the locale used to load object translations != current request locale
+     *
+     * @return string
+     */
+    public function getTranslatableLocale(AdminInterface $admin)
+    {
+        if ($this->translatableLocale == null) {
+            if ($admin->getRequest()) {
+                $this->translatableLocale = $admin->getRequest()->get(self::TRANSLATABLE_LOCALE_PARAMETER);
+            }
+            if ($this->translatableLocale == null) {
+                $this->translatableLocale = $this->getDefaultTranslationLocale($admin);
+            }
+        }
+
+        return $this->translatableLocale;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPersistentParameters(AdminInterface $admin)
+    {
+        return array(self::TRANSLATABLE_LOCALE_PARAMETER => $this->getTranslatableLocale($admin));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function alterNewInstance(AdminInterface $admin, $object)
+    {
+        if ($object->getLocale() === null) {
+            $object->setLocale($this->getTranslatableLocale($admin));
+        }
+    }
+
+    /**
+     * Check if $object is translatable
+     *
+     * @param  mixed $object
+     * @return bool
+     */
+    protected function isTranslatable($object)
+    {
+        if (is_null($object)) {
+            return false;
+        }
+
+        return (
+            in_array(
+                'Sonata\TranslationBundle\Model\TranslatableInterface',
+                class_implements($object)
+            )
+        );
+    }
+}
