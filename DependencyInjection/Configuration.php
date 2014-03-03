ea8fe5ba35de213029fefaacda89cf509bebc9b4
Nicolas Bastien <nbastien@prestaconcept.net>
2014-03-03 12:41:27 +0100

    Bundle set-up

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,16 +7,13 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Sonata\TranslationBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
  */
 class Configuration implements ConfigurationInterface
 {
