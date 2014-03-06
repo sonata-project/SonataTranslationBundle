840a3ef96ee254eef870d30f2271ad0429c68f5b
Nicolas Bastien <nbastien@prestaconcept.net>
2014-03-06 13:08:47 +0100

    Implement PHPCR

---
+++ Admin/Extension/Phpcr/TranslatableAdminExtension.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sonata\TranslationBundle\Admin\Extension\Phpcr;
+
+use Doctrine\ODM\PHPCR\DocumentManager;
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Sonata\TranslationBundle\Admin\Extension\AbstractTranslatableAdminExtension;
+
+/**
+ * @author Nicolas Bastien <nbastien.pro@gmail.com>
+ */
+class TranslatableAdminExtension extends AbstractTranslatableAdminExtension
+{
+    /**
+     * @param AdminInterface $admin
+     *
+     * @return DocumentManager
+     */
+    protected function getDocumentManager(AdminInterface $admin)
+    {
+        return $admin->getModelManager()->getDocumentManager();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function alterObject(AdminInterface $admin, $object)
+    {
+        $locale = $this->getTranslatableLocale($admin);
+
+        if ($this->isTranslatable($object) && ($object->getLocale() != $locale)) {
+            $object = $this->getDocumentManager($admin)->findTranslation($admin->getClass(), $object->getId(), $locale);
+        }
+    }
+}
