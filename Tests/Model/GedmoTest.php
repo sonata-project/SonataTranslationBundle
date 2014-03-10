45e8daa25a33c1e506d27cb3b77985004c6466b5
Nicolas Bastien <nbastien@prestaconcept.net>
2014-03-10 14:25:17 +0100

    Unit test for gedmo models and traits

---
+++ Tests/Model/GedmoTest.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sonata\AdminBundle\Tests\Model;
+
+use Sonata\TranslationBundle\Model\Gedmo\AbstractPersonalTranslatable;
+use Sonata\TranslationBundle\Model\Gedmo\AbstractPersonalTranslation;
+use Sonata\TranslationBundle\Model\Gedmo\AbstractTranslatable;
+use Sonata\TranslationBundle\Model\Gedmo\TranslatableInterface;
+
+
+class ModelTranslatable extends AbstractTranslatable implements TranslatableInterface
+{
+}
+
+class ModelPersonalTranslatable extends AbstractPersonalTranslatable implements TranslatableInterface
+{
+}
+
+class ModelPersonalTranslation extends AbstractPersonalTranslation
+{
+}
+
+/**
+ * @author Nicolas Bastien <nbastien.pro@gmail.com>
+ */
+class GedmoTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test AbstractTranslatable
+     */
+    public function testTranslatableModel()
+    {
+        $model = new ModelTranslatable();
+        $model->setLocale('fr');
+
+        $this->assertEquals('fr', $model->getLocale());
+        $this->assertTrue($model instanceof \Sonata\TranslationBundle\Model\TranslatableInterface);
+    }
+
+    /**
+     * @test AbstractPersonalTranslatable and AbstractPersonalTranslation
+     */
+    public function testPersonalTranslatableModel()
+    {
+        $model = new ModelPersonalTranslatable();
+        $model->setLocale('fr');
+
+        $this->assertEquals('fr', $model->getLocale());
+        $this->assertTrue($model instanceof \Sonata\TranslationBundle\Model\TranslatableInterface);
+
+        $model->addTranslation(new ModelPersonalTranslation('en', 'title', 'Title en'));
+        $model->addTranslation(new ModelPersonalTranslation('it', 'title', 'Title it'));
+        $model->addTranslation(new ModelPersonalTranslation('es', 'title', 'Title es'));
+
+        $this->assertEquals('Title en', $model->getTranslation('title', 'en'));
+        $this->assertEquals('Title it', $model->getTranslation('title', 'it'));
+        $this->assertEquals('Title es', $model->getTranslation('title', 'es'));
+
+        $this->assertEquals(3, count($model->getTranslations()));
+    }
+}
---
+++ Tests/Traits/GedmoTest.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sonata\AdminBundle\Tests\Traits;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Sonata\TranslationBundle\Model\Gedmo\AbstractPersonalTranslation;
+use Sonata\TranslationBundle\Model\Gedmo\TranslatableInterface;
+use Sonata\TranslationBundle\Traits\Gedmo\PersonalTranslatable;
+use Sonata\TranslationBundle\Traits\Translatable;
+
+
+class ModelTranslatable implements TranslatableInterface
+{
+    use Translatable;
+}
+
+class ModelPersonalTranslatable implements TranslatableInterface
+{
+    use PersonalTranslatable;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $translations;
+
+    public function __construct()
+    {
+        $this->translations = new ArrayCollection();
+    }
+}
+
+class ModelPersonalTranslation extends AbstractPersonalTranslation
+{
+}
+
+/**
+ * @author Nicolas Bastien <nbastien.pro@gmail.com>
+ */
+class GedmoTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test Translatable
+     */
+    public function testTranslatableModel()
+    {
+        $model = new ModelTranslatable();
+        $model->setLocale('fr');
+
+        $this->assertEquals('fr', $model->getLocale());
+        $this->assertTrue($model instanceof \Sonata\TranslationBundle\Model\TranslatableInterface);
+    }
+
+    /**
+     * @test PersonalTranslatable
+     */
+    public function testPersonalTranslatableModel()
+    {
+        $model = new ModelPersonalTranslatable();
+        $model->setLocale('fr');
+
+        $this->assertEquals('fr', $model->getLocale());
+        $this->assertTrue($model instanceof \Sonata\TranslationBundle\Model\TranslatableInterface);
+
+        $model->addTranslation(new ModelPersonalTranslation('en', 'title', 'Title en'));
+        $model->addTranslation(new ModelPersonalTranslation('it', 'title', 'Title it'));
+        $model->addTranslation(new ModelPersonalTranslation('es', 'title', 'Title es'));
+
+        $this->assertEquals('Title en', $model->getTranslation('title', 'en'));
+        $this->assertEquals('Title it', $model->getTranslation('title', 'it'));
+        $this->assertEquals('Title es', $model->getTranslation('title', 'es'));
+
+        $this->assertEquals(3, count($model->getTranslations()));
+    }
+}
