cc31ade654dbfda45bc5860cb0f881865ab71485
Nicolas Bastien <nbastien@prestaconcept.net>
2015-03-17 10:55:35 +0100

    Test Translatable checker

---
+++ Tests/Checker/TranslatableCheckerTest.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sonata\AdminBundle\Tests\Checker;
+
+use Sonata\TranslationBundle\Checker\TranslatableChecker;
+use Sonata\TranslationBundle\Model\AbstractTranslatable;
+use Sonata\TranslationBundle\Model\TranslatableInterface;
+use Sonata\TranslationBundle\Traits\Translatable;
+
+class ModelTranslatable extends AbstractTranslatable implements TranslatableInterface
+{
+}
+
+class ModelCustomTranslatable
+{
+}
+
+class ModelUsingTraitTranslatable
+{
+    use Translatable;
+}
+
+/**
+ * @author Nicolas Bastien <nbastien.pro@gmail.com>
+ */
+class TranslatableCheckerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test TranslatableChecker::isTranslatable
+     */
+    public function testIsTranslatableOnInterface()
+    {
+        $translatableChecker = new TranslatableChecker();
+
+        $object = new ModelTranslatable();
+
+        $this->assertFalse($translatableChecker->isTranslatable($object));
+
+        $translatableChecker->setSupportedInterfaces(array(
+            'Sonata\TranslationBundle\Model\TranslatableInterface'
+        ));
+
+        $this->assertTrue($translatableChecker->isTranslatable($object));
+    }
+
+    /**
+     * @test TranslatableChecker::isTranslatable
+     */
+    public function testIsTranslatableOnModel()
+    {
+        $translatableChecker = new TranslatableChecker();
+
+        $object = new ModelCustomTranslatable();
+
+        $this->assertFalse($translatableChecker->isTranslatable($object));
+
+        $translatableChecker->setSupportedModels(array(
+            'Sonata\AdminBundle\Tests\Checker\ModelCustomTranslatable'
+        ));
+
+        $this->assertTrue($translatableChecker->isTranslatable($object));
+    }
+
+    /**
+     * @test TranslatableChecker::isTranslatable
+     */
+    public function testIsTranslatableOnTrait()
+    {
+        $translatableChecker = new TranslatableChecker();
+
+        $object = new ModelUsingTraitTranslatable();
+
+        $this->assertTrue($translatableChecker->isTranslatable($object));
+    }
+}
