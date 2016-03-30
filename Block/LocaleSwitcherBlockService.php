d013efa673196cd993dc54dd3e42389089152675
David Buchmann <david.buchmann@liip.ch>
2016-03-30 14:37:29 +0200

    make block settings work with older versions of sonata block bundle too

--- Block/LocaleSwitcherBlockService.php
+++ Block/LocaleSwitcherBlockService.php
@@ -15,12 +15,21 @@ use Sonata\BlockBundle\Block\BaseBlockService;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * @author Nicolas Bastien <nbastien.pro@gmail.com>
  */
 class LocaleSwitcherBlockService extends BaseBlockService
 {
+    /**
+     * @deprecated Will be removed when upgrading to SonataBlockBundle 3
+     */
+    public function setDefaultSettings(OptionsResolverInterface $resolver)
+    {
+        $this->configureSettings($resolver);
+    }
+
     /**
      * {@inheritdoc}
      */
