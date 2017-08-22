251a3a8a3a0243d2ed4f147718639eae65afb1f9
Oskar Stark <oskarstark@googlemail.com>
2017-08-22 08:16:41 +0200

    added test for SonataTranslationExtension

---
+++ Tests/DependencyInjection/SonataTranslationExtensionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\TranslationBundle\Tests\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Sonata\TranslationBundle\DependencyInjection\SonataTranslationExtension;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class SonataTranslationExtensionTest extends AbstractExtensionTestCase
+{
+    public function testLoadServiceDefinitionWhenSonataDoctrineORMAdminBundleBundleIsRegistered()
+    {
+        $this->container->setParameter('kernel.bundles', array('SonataDoctrineORMAdminBundle' => 'whatever'));
+        $this->load();
+        $this->assertContainerBuilderHasService(
+            'sonata_translation.checker.translatable',
+            'Sonata\TranslationBundle\Checker\TranslatableChecker'
+        );
+        $this->assertContainerBuilderHasService(
+            'sonata_translation.filter.type.translation_field',
+            'Sonata\TranslationBundle\Filter\TranslationFieldFilter'
+        );
+    }
+
+    protected function getContainerExtensions()
+    {
+        return array(new SonataTranslationExtension());
+    }
+}
