6fcd32532a08e1fb65b94bd8dbb4b90f8277dd86
Jonathan Vautrin <jvautrin@pro-info.be>
2018-12-02 21:09:17 +0100

    Implement locale switcher (#251)

---
+++ src/EventSubscriber/UserLocaleSubscriber.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\TranslationBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+
+/**
+ * Stores the locale of the user in the session after the
+ * login. This can be used by the LocaleSubscriber afterwards.
+ *
+ * @author Jonathan Vautrin <jvautrin@pro-info.be>
+ */
+final class UserLocaleSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * @param SessionInterface $session
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param InteractiveLoginEvent $event
+     */
+    public function onInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if (null !== $user->getLocale()) {
+            $this->session->set('_locale', $user->getLocale());
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            SecurityEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin',
+        ];
+    }
+}
---
+++ tests/Action/SwitchLocaleActionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\TranslationBundle\Tests\Action;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\TranslationBundle\Action\SwitchLocaleAction;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+
+/**
+ * @author Jonathan Vautrin <jvautrin@pro-info.be>
+ */
+class SwitchLocaleActionTest extends TestCase
+{
+    public function testSwitchLocaleAction()
+    {
+        $session = new Session(new MockArraySessionStorage());
+        $session->set('_locale', 'en');
+        $request = new Request();
+        $request->setSession($session);
+        $action = new SwitchLocaleAction();
+        $action($request, 'fr');
+        $this->assertSame('fr', $session->get('_locale'));
+    }
+}
