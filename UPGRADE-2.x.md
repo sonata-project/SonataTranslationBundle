48a71acf078fbf47be5bad16cb6d8c0078d0a5aa
Fran Moreno <franmomu@gmail.com>
2020-09-29 09:42:01 +0200

    Add support for BlockBundle 4 (#375)

--- UPGRADE-2.x.md
+++ UPGRADE-2.x.md
@@ -1,19 +1,26 @@
 UPGRADE 2.x
 ===========
 
+### Added support for sonata-project/block-bundle ^4.0
+
+If you are using `sonata-project/block-bundle`, you MUST declare explicitly the dependency with this package on your `composer.json` in order to avoid unwanted upgrades.
+
+There is a minimal BC break on `LocaleSwitcherBlockService`. If you are extending this class (keep in mind that
+it's marked as final and cannot be extended on 4.0) you SHOULD add return type declarations to `execute()` and `configureSettings()`.
+
 UPGRADE FROM 2.0 to 2.1
 =======================
 
 ### Tests
 
-All files under the ``Tests`` directory are now correctly handled as internal test classes. 
-You can't extend them anymore, because they are only loaded when running internal tests. 
+All files under the ``Tests`` directory are now correctly handled as internal test classes.
+You can't extend them anymore, because they are only loaded when running internal tests.
 More information can be found in the [composer docs](https://getcomposer.org/doc/04-schema.md#autoload-dev).
 
 ## Deprecated traits
 
-The `Sonata\TranslationBundle\Traits\Translatable` class is deprecated. 
+The `Sonata\TranslationBundle\Traits\Translatable` class is deprecated.
 Use `Sonata\TranslationBundle\Traits\TranslatableTrait` instead.
 
-The `Sonata\TranslationBundle\Traits\PersonalTranslatable` class is deprecated. 
+The `Sonata\TranslationBundle\Traits\PersonalTranslatable` class is deprecated.
 Use `Sonata\TranslationBundle\Traits\PersonalTranslatableTrait` instead.
---
+++ tests/Block/LocaleSwitcherBlockServiceTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\TranslationBundle\Tests\Block;
+
+use Sonata\BlockBundle\Test\BlockServiceTestCase;
+use Sonata\TranslationBundle\Block\LocaleSwitcherBlockService;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
+use Twig\Environment;
+
+final class LocaleSwitcherBlockServiceTest extends BlockServiceTestCase
+{
+    use ExpectDeprecationTrait;
+
+    public function testDefaultSettings(): void
+    {
+        $environment = $this->createStub(Environment::class);
+
+        $showCountryFlags = true;
+        $localeSwitcherBlock = new LocaleSwitcherBlockService($environment, $showCountryFlags);
+
+        $blockContext = $this->getBlockContext($localeSwitcherBlock);
+
+        $this->assertSettings([
+            'admin' => null,
+            'object' => null,
+            'template' => '@SonataTranslation/Block/block_locale_switcher.html.twig',
+            'locale_switcher_route' => null,
+            'locale_switcher_route_parameters' => [],
+            'locale_switcher_show_country_flags' => $showCountryFlags,
+        ], $blockContext);
+    }
+
+    /**
+     * NEXT_MAJOR: Remove this test.
+     *
+     * @group legacy
+     */
+    public function testDefaultSettingsWithWrongConstructor(): void
+    {
+        $this->expectException(\TypeError::class);
+        new LocaleSwitcherBlockService($this->createStub(Environment::class), new \stdClass());
+    }
+
+    /**
+     * NEXT_MAJOR: Remove this test.
+     *
+     * @group legacy
+     */
+    public function testDefaultSettingsWithDeprecatedConstructor(): void
+    {
+        $environment = $this->createStub(Environment::class);
+
+        $showCountryFlags = true;
+        $this->expectDeprecation(
+            'Passing "null" as argument 2 to "Sonata\TranslationBundle\Block\LocaleSwitcherBlockService::__construct()"'
+            .' is deprecated since sonata-project/translation-bundle 2.x and will throw a "TypeError" error in version 3.0.'
+            .' You must pass a "bool" value instead.'
+        );
+        $localeSwitcherBlock = new LocaleSwitcherBlockService($environment, null, $showCountryFlags);
+
+        $blockContext = $this->getBlockContext($localeSwitcherBlock);
+
+        $this->assertSettings([
+            'admin' => null,
+            'object' => null,
+            'template' => '@SonataTranslation/Block/block_locale_switcher.html.twig',
+            'locale_switcher_route' => null,
+            'locale_switcher_route_parameters' => [],
+            'locale_switcher_show_country_flags' => $showCountryFlags,
+        ], $blockContext);
+    }
+}
