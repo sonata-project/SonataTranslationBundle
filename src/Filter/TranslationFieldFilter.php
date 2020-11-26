7ca8f9e95491cd26c01e23b11cc8fc5ecc2fd551
Fran Moreno <franmomu@gmail.com>
2020-11-26 18:35:59 +0100

    Add mixed param declaration

--- src/Filter/TranslationFieldFilter.php
+++ src/Filter/TranslationFieldFilter.php
@@ -33,6 +33,9 @@ final class TranslationFieldFilter extends Filter
         $this->filterMode = $filterMode;
     }
 
+    /**
+     * @param mixed $value
+     */
     public function filter(ProxyQueryInterface $queryBuilder, string $alias, string $field, $value): void
     {
         if (!$value || !\is_array($value) || !\array_key_exists('value', $value) || null === $value['value']) {
