41b6f755b94367f60505b8edacf81010a5662a58
Fran Moreno <franmomu@gmail.com>
2021-04-19 02:16:26 +0200

    Drop support for knplabs/doctrine-behaviors <2.0

--- tests/Fixtures/Model/Knplabs/TranslatableEntity.php
+++ tests/Fixtures/Model/Knplabs/TranslatableEntity.php
@@ -14,59 +14,30 @@ declare(strict_types=1);
 namespace Sonata\TranslationBundle\Tests\Fixtures\Model\Knplabs;
 
 use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface as KNPTranslatableInterface;
-use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
 use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
 use Sonata\TranslationBundle\Model\TranslatableInterface;
 
-// @todo Remove check and else part when dropping support for knplabs/doctrine-behaviors < 2.0
-if (interface_exists(KNPTranslatableInterface::class)) {
-    class TranslatableEntity implements TranslatableInterface, KNPTranslatableInterface
+class TranslatableEntity implements TranslatableInterface, KNPTranslatableInterface
+{
+    use TranslatableTrait;
+
+    /**
+     * @var int|null
+     */
+    private $id;
+
+    public function getId(): ?int
     {
-        use TranslatableTrait;
-
-        /**
-         * @var int|null
-         */
-        private $id;
-
-        public function getId(): ?int
-        {
-            return $this->id;
-        }
-
-        public function setLocale($locale): void
-        {
-            $this->setCurrentLocale($locale);
-        }
-
-        public function getLocale(): string
-        {
-            return $this->getCurrentLocale();
-        }
+        return $this->id;
     }
-} else {
-    class TranslatableEntity implements TranslatableInterface
+
+    public function setLocale($locale): void
     {
-        use Translatable;
+        $this->setCurrentLocale($locale);
+    }
 
-        /**
-         * @var int|null
-         */
-        private $id;
-
-        public function getId(): ?int
-        {
-            return $this->id;
-        }
-
-        public function setLocale($locale): void
-        {
-            $this->setCurrentLocale($locale);
-        }
-
-        public function getLocale(): string
-        {
-            return $this->getCurrentLocale();
-        }
+    public function getLocale(): string
+    {
+        return $this->getCurrentLocale();
     }
 }
