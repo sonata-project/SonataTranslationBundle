3ecfb1dbc9482dcb2fb649d2097c2815de93a9bc
Fran Moreno <franmomu@gmail.com>
2021-10-09 10:33:25 +0200

    Allow phpstan understand object manager

--- tests/App/Entity/KnpCategoryTranslation.php
+++ tests/App/Entity/KnpCategoryTranslation.php
@@ -34,7 +34,7 @@ class KnpCategoryTranslation implements TranslationInterface
     /**
      * @var string|null
      *
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $name;
 
