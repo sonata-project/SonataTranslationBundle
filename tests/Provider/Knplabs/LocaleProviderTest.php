4ef01eaef7da3af8e47b4f109527dc1d71d7ad87
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-09-28 18:27:54 +0200

    Fix

--- tests/Provider/Knplabs/LocaleProviderTest.php
+++ tests/Provider/Knplabs/LocaleProviderTest.php
@@ -23,7 +23,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 final class LocaleProviderTest extends TestCase
 {
     /**
-     * @dataProvider provideValues
+     * @dataProvider provideUsesTheProperProviderBasedOnRequestCases
      */
     public function testUsesTheProperProviderBasedOnRequest(
         string $expectedLocale,
@@ -53,7 +53,7 @@ final class LocaleProviderTest extends TestCase
     /**
      * @return iterable<array{string, string, string, bool}>
      */
-    public function provideValues(): iterable
+    public function provideUsesTheProperProviderBasedOnRequestCases(): iterable
     {
         yield 'with sonata enabled it uses sonata provider' => ['en', 'es', 'en', true];
         yield 'with sonata disabled it uses knp provider' => ['es', 'es', 'en', false];
